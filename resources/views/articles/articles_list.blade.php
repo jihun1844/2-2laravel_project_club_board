44e57c118fbbb5ead8fbfc6add4921968de32aaf
jihun <tohka113@naver.com>
2023-10-26 22:57:09 +0900

    welcome.blade CSS edit

--- resources/views/articles/articles_list.blade.php
+++ resources/views/articles/articles_list.blade.php
@@ -4,12 +4,55 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> --}}
   <title>Document</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+      margin: 0;
+      padding: 20px;
+    }
+    
+    .table {
+      width: 100%;
+      border-collapse: collapse;
+      background-color: #fff;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+    
+    .table thead tr {
+      background-color: #f1f1f1;
+    }
+    
+    .table th,
+    .table td {
+      padding: 10px;
+      text-align: left;
+      border-bottom: 1px solid #ddd;
+    }
+    
+    .table a {
+      color: #333;
+      text-decoration: none;
+    }
+    
+    .button {
+      display: inline-block;
+      margin-right: 10px;
+      padding: 10px 20px;
+      background-color: #9bc5f3;
+      color: #fff;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      text-decoration: none;
+    }
+    
+    .button:hover {
+      background-color: #0056b3;
+    }
+  </style>
 </head>
-<style>
- 
-</style>
 <body>
   <table class="table">
     <thead>
@@ -18,32 +61,25 @@
         <th scope="col">제목</th>
         <th scope="col">작성자</th>
         <th scope="col">모이는 날</th>
-        
       </tr>
     </thead>
-    {{-- <tbody class="table-group-divider"> --}}
-    <tbody >
+    <tbody>
       @foreach($articles as $article)
       <tr>
         <td>{{$loop->index+1}}</td>
         <td><a href="/articles/{{$article->id}}">{{$article->title}}</a></td>
         <td>{{$article->user_id}}</td>
-        {{-- <td>{{$article->created_at}}</td> --}}
       </tr>
       @endforeach
     </tbody>
   </table>
+  
   @if(Auth::check())
-  {{-- class="btn btn-outline-primary" --}}
-  <button type="button"><a href="/articles/create">글쓰기</a></button>
-    
+    <button class="button"><a href="/articles/create">글쓰기</a></button>
   @else
-  {{-- class="btn btn-outline-success" --}}
-    <button type="button" ><a href="login">로그인후 글쓰러 가기</a></button>
-    
-    {{-- 여기로 로그인후 다시 글 리스트로 돌아오는 코드 해야함 --}}
+    <button class="button"><a href="login">로그인후 글쓰러 가기</a></button>
   @endif
-  {{-- class="btn btn-outline-success" --}}
-  <button type="button" ><a href="/">HOME으로 가기</a></button>
+  
+  <button class="button"><a href="/">HOME으로 가기</a></button>
 </body>
 </html>
